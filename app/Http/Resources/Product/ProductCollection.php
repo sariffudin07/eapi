23cfe76634232408c226a8a2cef397f344e9696f
paw <pawpraktikum@gmail.com>
2022-09-06 14:53:06 +0700

    Product Collection Resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use App\Http\Resources\Product\ProductResource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends ProductResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,15 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 1),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'discount' => $this->discount,
+            'href' => [
+                'link' => route('products.show', $this->id)
+            ]
+            ];
     }
 }
